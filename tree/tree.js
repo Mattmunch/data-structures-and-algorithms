1c395d82acfce12edf0dd9a2df43f25d5b919228
Matt Munch <mattmunch94@gmail.com>
2020-01-28 13:31:04 -0800

    insert and insertNode methods created

--- tree/tree.js
+++ tree/tree.js
@@ -9,5 +9,21 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
-    
+    insert(value) {
+        const newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+        } else this.insertNode(this.root, newNode);
+    }
+    insertNode(node, newNode) {
+        if(newNode.value < node.value) {
+            if(node.left === null) {
+                node.left = newNode;
+            } else this.insertNode(node.left, newNode);
+        } else {
+            if(node.right === null) {
+                node.right = newNode;
+            } else this.insertNode(node.right, newNode);
+        }
+    }
 }
