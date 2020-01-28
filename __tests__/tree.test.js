38ce561bce711111532f301ef8a74f3607922832
Matt Munch <mattmunch94@gmail.com>
2020-01-28 14:06:50 -0800

    2 passing tests

--- __tests__/tree.test.js
+++ __tests__/tree.test.js
@@ -4,6 +4,10 @@ describe('BinarySearchTree', () => {
     const tree = new BinarySearchTree();
     expect(tree.root).toEqual(null);
   });
-    
+  it('Can successfully instantiate a tree with a single root node', () => {
+    const tree = new BinarySearchTree();
+    tree.add('123');
+    expect(tree.root).toEqual({ left: null, right: null, value: '123' });
+  });
 })
 ;
